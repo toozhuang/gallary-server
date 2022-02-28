89d242bd1fac73ca27e24978bf914486a65e89e8
toozhuang <wangz.today@gmail.com>
2022-02-28 11:46:48 +0800

    feat: 添加 json 作为 db

--- src/gallery/gallery.controller.ts
+++ src/gallery/gallery.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { ReadFileService } from './readFile.service';
+import { join } from 'path';
+import * as fs from 'fs';
 
 @Controller('gallery')
 export class GalleryController {
   constructor(private readonly readFileService: ReadFileService) {}
 
   @Get()
-  getHello(): Promise<any> {
-    return this.readFileService.readFolder('/Users/wang/Documents');
+  async getHello(): Promise<{ name: string; poster: string; meta: string; fanart: string }[]> {
+    console.log(process.cwd());
+    const jsonDb = await fs.promises.readFile(join(process.cwd(), 'json-db/movie.json'), 'utf8');
+    // console.log(jsonDb);
+    const result = JSON.parse(jsonDb);
+    // return new StreamableFile(jsonDb);
+    return new Promise((resolve, reject) => {
+      resolve(result);
+      reject(result);
+    });
+    // return this.readFileService.readFolder('/Users/wang/Documents');
   }
 }
