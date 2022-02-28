fb414875368191a03e710b1dd970ee0bed52fe6f
toozhuang <wangz.today@gmail.com>
2022-02-28 16:56:31 +0800

    config: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "no-warning-comments": [0, { "terms": ["todo", "fixme", "xxx"], "location": "start" }], //不能有警告备注
   },
 };
