b12e2bee537e01c1f08658adbb42a9ef710eec2f
toozhuang <wangz.today@gmail.com>
2022-03-01 22:45:19 +0800

    feat: 读取 nfo 信息（兼容 TinyMedia），并写入 json 数据库。

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,6 +7,7 @@ import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
+import { XMLParser } from 'fast-xml-parser';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { MovieDb } = require('moviedb-promise');
 @Injectable()
@@ -20,28 +21,32 @@ export class AppService {
   getMovieDb() {
     return this.moviedb;
   }
+  // const jsonDb = await fs.promises.readFile(join(process.cwd(), 'json-db/movie.json'), 'utf8');
+  // // console.log(jsonDb);
+  // const result = JSON.parse(jsonDb);
+  // console.log(result);
+  // const XMLdata = await fs.promises.readFile(
+  //   join(process.cwd(), 'json-db/非常警探.Restless.2022.WEB-DL.1080p.X264.nfo'),
+  // );
+  // const parser = new XMLParser();
+  // const jObj = parser.parse(XMLdata);
+  // result.push(jObj.movie);
+  //
+  // await fs.promises.writeFile(join(process.cwd(), 'json-db/test.json'), JSON.stringify(result));
 
-  setConfiguration() {
-    this.moviedb.configuration().then(async (value) => {
-      let accessAble: boolean;
-      try {
-        await fs.promises.access(
-          join(process.cwd(), 'json-db/configure.json'),
-          fs.constants.R_OK | fs.constants.W_OK,
-        );
-        accessAble = true;
-      } catch {
-        accessAble = false;
-      }
-      console.log(accessAble);
-      if (!accessAble) {
+  async setConfiguration() {
+    try {
+      await fs.promises.access(
+        join(process.cwd(), 'json-db/configure.json'),
+        fs.constants.R_OK | fs.constants.W_OK,
+      );
+    } catch {
+      this.moviedb.configuration().then(async (value) => {
         await fs.promises.writeFile(
           join(process.cwd(), 'json-db/configure.json'),
           JSON.stringify(value),
         );
-      } else {
-        console.log('文件已经存在， 不需要了； ');
-      }
-    });
+      });
+    }
   }
 }
--- src/gallery/gallery.controller.ts
+++ src/gallery/gallery.controller.ts
@@ -4,6 +4,9 @@ import { Request } from 'express';
 import { join } from 'path';
 import * as fs from 'fs';
 import { AppService } from '../app.service';
+import { XMLParser } from 'fast-xml-parser';
+import * as path from 'path';
+import * as _ from 'lodash';
 
 @Controller('gallery')
 export class GalleryController {
@@ -11,6 +14,55 @@ export class GalleryController {
     private readonly readFileService: ReadFileService,
     private readonly movieDbService: AppService,
   ) {}
+
+  @Get('scanner')
+  async scannerFolder(folder = '/Volumes/My Passport/非常警探') {
+    folder = `/Volumes/My Passport`;
+    const moviesFolder = await fs.promises.readdir(folder, 'utf8');
+    //  读取 meta 文件， 并存储到项目数据库
+    const jsonDb = await fs.promises.readFile(join(process.cwd(), 'json-db/movie.json'), 'utf8');
+    // console.log(jsonDb);
+    const jsonDbObj = JSON.parse(jsonDb);
+
+    for (let index = 0; index < moviesFolder.length; index++) {
+      const movieFolder = moviesFolder[index];
+      // console.log(movieFolder);
+      try {
+        // 是文件
+        const isFolder = fs.lstatSync(path.join(folder, `/${movieFolder}`)).isDirectory();
+        if (isFolder) {
+          const movieFolderItemList = await fs.promises.readdir(
+            path.join(folder, `/${movieFolder}`),
+            'utf8',
+          );
+
+          const extName = movieFolderItemList.map((item) => path.extname(item));
+          const nfoInxde = _.indexOf(extName, '.nfo');
+          if (nfoInxde > 0) {
+            const XMLdata = await fs.promises.readFile(
+              join(`${folder}/${movieFolder}/`, movieFolderItemList[nfoInxde]),
+            );
+            const parser = new XMLParser();
+            const jObj = parser.parse(XMLdata);
+            jsonDbObj.push(jObj.movie);
+
+            await fs.promises.writeFile(
+              join(process.cwd(), 'json-db/movie.json'),
+              JSON.stringify(jsonDbObj),
+            );
+          } else {
+            // todo: 不包含 meta 文件， 当前情况则跳过
+          }
+        } else {
+          console.log(folder, `/${movieFolder}`, 'is not a folder');
+        }
+      } catch (error) {
+        //  是文件夹
+      }
+    }
+
+    return 'aoaojiao';
+  }
 
   @Get()
   async getHello(
