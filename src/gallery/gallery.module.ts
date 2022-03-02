7b79166643d44e15835212ca176b21a717f2959e
toozhuang <wangz.today@gmail.com>
2022-03-02 23:56:01 +0800

    bump

--- src/gallery/gallery.module.ts
+++ src/gallery/gallery.module.ts
@@ -2,18 +2,18 @@ import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/c
 import { GalleryService } from './gallery.service';
 import { GalleryController } from './gallery.controller';
 import { ReadFileService } from './readFile.service';
-import { AppService } from '../app.service';
+import { MovieService } from '../movie.service';
 
 import { MoviedbConfigureMiddleware } from './moviedbConfigure.middleware';
 
 import { XMLParser, XMLBuilder, XMLValidator } from 'fast-xml-parser';
 
 @Module({
-  providers: [GalleryService, ReadFileService, AppService],
+  providers: [GalleryService, ReadFileService, MovieService],
   controllers: [GalleryController],
 })
 export class GalleryModule implements NestModule {
-  constructor(movieService: AppService) {
+  constructor(movieService: MovieService) {
     // note: 获取 configure 配置信息，
     // 并讲该信息以 json 的方式写入到 文件夹中
     // 只有后续再出错的情况下才去更新该配置文件
