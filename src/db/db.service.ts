002a5815f0cd45994ba19c2f8e01ac561921ecbb
toozhuang <wangz.today@gmail.com>
2022-03-17 23:31:36 +0800

    feat: 降级lowdb 到1.0

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
+const Low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
 @Injectable()
-export class DbService {}
+export class DbService {
+  async init() {
+    const adapter = new FileSync('./json-db/movieDatabase.json');
+    const db = new Low(adapter);
+    db.read();
+    console.log(db);
+
+    console.log(db.get('movies').value());
+  }
+}
