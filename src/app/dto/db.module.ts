73c8a54785dde41acfb97758f958f0e17f2c0c89
toozhuag <wangz.today@gmail.com>
2022-03-23 21:55:08 +0800

    refactor: delete unneeded files

--- src/app/dto/db.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DbService } from './db.service';
-
-@Module({
-  providers: [DbService],
-  controllers: [],
-  exports: [DbService],
-})
-export class DbModule {}
