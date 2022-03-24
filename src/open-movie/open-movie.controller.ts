70bd973fc4d9df0a26450c26737605c5e7df57c0
toozhuag <wangz.today@gmail.com>
2022-03-24 23:58:12 +0800

    feat: open movie api - search 接口

--- src/open-movie/open-movie.controller.ts
+++ src/open-movie/open-movie.controller.ts
@@ -1,15 +1,17 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { OpenMovieService } from './open-movie.service';
 
 @Controller('open-movie')
 export class OpenMovieController {
+  constructor(private readonly openMovieService: OpenMovieService) {}
+
   @Get('all')
   async galleryAll() {
     return 'aoaoatest';
   }
 
   @Post('name')
-  searchMovieByName(@Body('name') movieName) {
-    console.log(movieName);
-    return 'k';
+  async searchMovieByName(@Body('name') movieName) {
+    return this.openMovieService.searchMovie(movieName);
   }
 }
--- src/open-movie/open-movie.service.ts
+++ src/open-movie/open-movie.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { TmdbService } from '../services/tmdb/tmdb.service';
+import { DbService } from '../services/db/db.service';
 
 @Injectable()
-export class OpenMovieService {}
+export class OpenMovieService {
+  constructor(private tmdService: TmdbService, private serviceh: DbService) {}
+
+  async searchMovie(movieName) {
+    const moviedb = this.tmdService.tmDB;
+    try {
+      const res = await moviedb.searchMovie({ query: movieName });
+      return res;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
