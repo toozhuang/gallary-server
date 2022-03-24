159dc85ba5e5ea625e99358afaf563652535b5c8
toozhuag <wangz.today@gmail.com>
2022-03-24 23:55:24 +0800

    feat: 添加 the movie db 相关的服务

---
+++ src/services/tmdb/tmdb.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TmdbService } from './tmdb.service';
+
+@Module({
+  providers: [TmdbService],
+  exports: [TmdbService],
+})
+export class TmdbModule {
+  constructor(private readonly tmdbService: TmdbService) {
+    tmdbService.init();
+  }
+}
