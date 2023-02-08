7a7a548cfb840861ae2c0dd1ccdbe114f0761b1f
toozhuag <wangz.today@gmail.com>
2023-02-08 16:48:23 +0800

    feat: set up Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Wang's Box example")
+    .setDescription("The Wang's Box API description")
+    .setVersion('1.0')
+    .addTag('box')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors();
   await app.listen(8080);
 }
+
 bootstrap();
